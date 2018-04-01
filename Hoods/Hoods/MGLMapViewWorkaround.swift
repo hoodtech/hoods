2aae943985c8762f219411f33426e559c34d2fdd
appdrew <andrew@appdrew.com>
2018-03-31 23:13:23 -0400

    Added MGLMapView subclass with NSCoder init as a provided workaround for a storyboard crash that was introduced when Mapbox made minimum and maximumZoomDistance IBInspectable

---
+++ Hoods/Hoods/MGLMapViewWorkaround.swift
@@ -0,0 +1,15 @@
+//
+//  MGLMapViewWorkaround.swift
+//  Hoods
+//
+//  Created by Andrew Carvajal on 3/31/18.
+//  Copyright © 2018 Hoods. All rights reserved.
+//
+
+import Mapbox
+
+class MGLMapViewWorkaround: MGLMapView {
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+}

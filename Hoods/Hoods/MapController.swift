5690273c339aaf02a59816ae679ca2855111be71
appdrew <andrew@appdrew.com>
2018-03-31 23:43:24 -0400

    Added print for didReceiveMemoryWarning()

--- Hoods/Hoods/MapController.swift
+++ Hoods/Hoods/MapController.swift
@@ -19,17 +19,6 @@ class MapController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        print("didReceiveMemoryWarning()")
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
